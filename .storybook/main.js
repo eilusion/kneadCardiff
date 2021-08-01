cca6fc9b6da15ca3c6d72d9fd39d04d179e6e1c4
Lewis Edwards <lewiseilusion@gmail.com>
2021-08-01 15:25:10 +0100

    added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/stories/**/*.stories.mdx",
+    "../src/components/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
