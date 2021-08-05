294ecb9a3a304f719333f6bead3dbff6d8de116a
Lewis Edwards <lewiseilusion@gmail.com>
2021-08-05 15:58:58 +0100

    bigbutton & yarn.lock

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,20 @@
 module.exports = {
   "stories": [
-    "../src/components/stories/**/*.stories.mdx",
-    "../src/components/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
+  ],
+  webpackFinal: async (config, { configType }) => {
+      config.module.rules.push({
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+          include: path.resolve(__dirname, '../'),
+      });
+
+      return config;
+  },
 }
---
+++ src/components/BigButton/BigButton.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BigButton } from "./BigButton";
+
+export default {
+    component: BigButton,
+    title: "Components/Big Button",
+};
+
+export const Default = () => {
+    return (
+        <BigButton
+            
+        />
+    );
+}
