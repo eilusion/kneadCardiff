a50ec20bce2fe36e83a9fcd206fc150d225da9f0
Lewis Edwards <lewiseilusion@gmail.com>
2021-11-26 11:35:44 +0000

    indenting fix

--- src/components/CheckoutHeader/CheckoutHeader.jsx
+++ src/components/CheckoutHeader/CheckoutHeader.jsx
@@ -10,20 +10,20 @@ export const CheckoutHeader = (props) => {
             <div className="CheckoutHeader__container">
                 <div className="CheckoutHeader__deliveryDining">
                     <Icon
-                    IconElement={Icons.DELIVERY_DINING}
-                    color={Colors.PRIMARY6}
+                        IconElement={Icons.DELIVERY_DINING}
+                        color={Colors.PRIMARY6}
                     />
                 </div>
                 <div className="CheckoutHeader__pizzaCounter">{props.pizzaCounter}</div>
-                <Icon
-                IconElement={Icons.PIZZA_ICON}
-                color={Colors.PRIMARY6}
-                />
+                    <Icon
+                        IconElement={Icons.PIZZA_ICON}
+                        color={Colors.PRIMARY6}
+                    />
                 <div className="CheckoutHeader__totalPrice">{props.totalPrice}</div>
                 <div className="CheckoutHeader__diningBorder">
                     <Icon
-                    IconElement={Icons.BASKET_ICON}
-                    color={Colors.PRIMARY6}
+                        IconElement={Icons.BASKET_ICON}
+                        color={Colors.PRIMARY6}
                     />
                 </div>
             </div>
