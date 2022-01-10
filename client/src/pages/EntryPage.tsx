38486669005a3593f2bbae85290637febc42c74c
Lewis Edwards <lewiseilusion@gmail.com>
2022-01-10 13:30:56 +0000

    client side compiler

---
+++ client/src/pages/EntryPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EntryPage.css";
+
+type TEntryPageProps = {}
+
+export const EntryPage = (props: React.PropsWithChildren<TEntryPageProps>): JSX.Element => {
+    return (
+        <div className="entry-page">
+            hello
+        </div>
+    );
+};
